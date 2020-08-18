6493e573aa9517d7001aeb6adf847185574c4d8a
@Maksim <makshc2@gmail.com>
2020-08-18 11:52:12 +0300

    fixed router

--- src/services/firebase/resetPassword.services.js
+++ src/services/firebase/resetPassword.services.js
@@ -1,12 +1,11 @@
 import firebase from '@/plugins/firebase';
 import router from '@/router';
+
 // eslint-disable-next-line import/prefer-default-export
 export const firebaseResetPassword = async (email) => {
   try {
-    const data = await firebase.auth().sendPasswordResetEmail(email)
-      .then(() => {
-        router.push({ name: 'Login' });
-      });
+    const data = await firebase.auth().sendPasswordResetEmail(email);
+    router.push({ name: 'Login' });
     return data;
   } catch (e) {
     return Promise.reject(e);
